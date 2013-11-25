49aa480498ec401aa0af532a0b893c29efbad0c1
Daniel Sundfeld <sund@unb.br>
2013-11-25 20:45:53 -0200

    Initial three-dimension coords class

---
+++ src/Coords.cpp
@@ -0,0 +1,9 @@
+#include "Coords.h"
+
+Coords::Coords(int x, int y, int z)
+{
+    x = x;
+    y = y;
+    z = z;
+}
+
---
+++ src/Coords.h
@@ -0,0 +1,10 @@
+#ifndef _COORDS_H
+#define _COORDS_H
+
+class Coords {
+    public:
+        int x, y, z;
+        Coords(int x, int y, int z);
+};
+
+#endif
