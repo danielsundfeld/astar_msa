1faab0af8128278cd0d67a52acf4e458ff646037
Daniel Sundfeld <sund@unb.br>
2013-11-26 10:04:46 -0200

    AStar variables

--- src/AStar.cpp
+++ src/AStar.cpp
@@ -1,7 +1,24 @@
+#include <iostream>
+#include <map>
+#include <queue>
+#include <string>
+#include <vector>
+#include <stdio.h>
+
+#include "Coord.h"
+#include "Node.h"
+#include "Sequences.h"
+
+using namespace std;
+
 #include "Node.h"
 
 int a_star()
 {
+    map<Coord, Node> OpenList;
+    map<Coord, Node> ClosedList;
+    priority_queue<Node, vector<Node>, PriorityNode> pq;
+
     return 0;
 }
 
