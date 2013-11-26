77ce1ea3c1c2eb03d38908d30bc754ee65c34e3c
Daniel Sundfeld <sund@unb.br>
2013-11-26 09:33:58 -0200

    Dont be a "this->" fanatic

--- src/Coord.cpp
+++ src/Coord.cpp
@@ -18,31 +18,31 @@ Coord& Coord::operator=(const Coord &rhs)
 {
     if (this == &rhs) // identify self-assignment
         return *this; // do nothing
-    this->m_x = rhs.m_x;
-    this->m_y = rhs.m_y;
-    this->m_z = rhs.m_z;
+    m_x = rhs.m_x;
+    m_y = rhs.m_y;
+    m_z = rhs.m_z;
 
     return *this;
 }
 
 int Coord::operator==(const Coord &rhs) const
 {
-    if (this->m_x != rhs.m_x)
+    if (m_x != rhs.m_x)
         return 0;
-    if (this->m_y != rhs.m_y)
+    if (m_y != rhs.m_y)
         return 0;
-    if (this->m_z != rhs.m_z)
+    if (m_z != rhs.m_z)
         return 0;
     return 1;
 }
 
 int Coord::operator<(const Coord &rhs) const
 {
-    if (this->m_x == rhs.m_x && this->m_y == rhs.m_y && this->m_z < rhs.m_z)
+    if (m_x == rhs.m_x && m_y == rhs.m_y && m_z < rhs.m_z)
         return 1;
-    if (this->m_x == rhs.m_x && this->m_y < rhs.m_y)
+    if (m_x == rhs.m_x && m_y < rhs.m_y)
         return 1;
-    if (this->m_x < rhs.m_x)
+    if (m_x < rhs.m_x)
         return 1;
     return 0;
 }
