27de24ca4196e0badb69781366836fe0367f5ca0
Daniel Sundfeld <sund@unb.br>
2013-11-27 10:15:30 -0200

    Infinite Loop fix and new documentation

--- src/AStar.cpp
+++ src/AStar.cpp
@@ -13,9 +13,30 @@ using namespace std;
 
 void print_coord(const char msg[], Node l)
 {
-    printf("%s (%d) (%d %d %d)/(%d)\n", msg, l.get_g(), l.pos.get_x(), l.pos.get_y(), l.pos.get_z(), l.get_h());
+    printf("%s (%d %d %d)/(g-%d,h-%d,f-%d)\n", msg, l.pos.get_x(), l.pos.get_y(), l.pos.get_z(), l.get_g(), l.get_h(), l.get_f());
 }
 
+/*
+ * A classic A-Star implentation resume:
+ *
+ * OPEN = priority queue with START node
+ * CLOSED = empty
+ *
+ * while lowest rank in OPEN is not the GOAL:
+ * current = remove lowest rank item from OPEN
+ * add current to CLOSED
+ * for neighbors of current:
+ *   cost = g(current) + movementcost(current, neighbor)
+ *   if neighbor in OPEN and cost less than g(neighbor):
+ *     remove neighbor from OPEN, because new path is better
+ *   if neighbor in CLOSED and cost less than g(neighbor):
+ *     remove neighbor from CLOSED
+ *   if neighbor not in OPEN and neighbor not in CLOSED:
+ *     set g(neighbor) to cost
+ *     add neighbor to OPEN
+ *     set priority queue rank to g(neighbor) + h(neighbor)
+ *     set neighbor's parent to current
+ */
 int a_star()
 {
     map<Coord, Node> OpenList;
@@ -43,12 +64,12 @@ int a_star()
             if ((search = OpenList.find(it->pos)) != OpenList.end())
             {
                 // if score on open list is better, ignore this neighboor
-                if (it->get_g() > search->second.get_g())
+                if (it->get_g() >= search->second.get_g())
                     continue;
             }
             if ((search = ClosedList.find(it->pos)) != ClosedList.end())
             {
-                if (it->get_g() > search->second.get_g())
+                if (it->get_g() >= search->second.get_g())
                     continue;
                 ClosedList.erase(it->pos);
             }
@@ -62,22 +83,3 @@ int a_star()
     return 0;
 }
 
-/*
-OPEN = priority queue containing START
-CLOSED = empty set
-while lowest rank in OPEN is not the GOAL:
-  current = remove lowest rank item from OPEN
-  add current to CLOSED
-  for neighbors of current:
-    cost = g(current) + movementcost(current, neighbor)
-    if neighbor in OPEN and cost less than g(neighbor):
-      remove neighbor from OPEN, because new path is better
-    if neighbor in CLOSED and cost less than g(neighbor): **
-      remove neighbor from CLOSED
-    if neighbor not in OPEN and neighbor not in CLOSED:
-      set g(neighbor) to cost
-      add neighbor to OPEN
-      set priority queue rank to g(neighbor) + h(neighbor)
-      set neighbor's parent to current
-
-*/
