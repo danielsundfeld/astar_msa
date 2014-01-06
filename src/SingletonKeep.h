95b3b70e5d670df03463383c5a4a06ad893d5913
Daniel Sundfeld <sund@unb.br>
2014-01-06 19:26:44 -0200

    SingletonKeep documentation

--- src/SingletonKeep.h
+++ src/SingletonKeep.h
@@ -1,15 +1,19 @@
 #ifndef _SINGLETONKEEP_H
 #define _SINGLETONKEEP_H
 /*!
+ * \filename SingletonKeep.h
+ * \author Daniel Sundfeld
+ */
+
+/*!
  * \brief Class that grants the singletons are created before main
  * starts and destroy when the program ends.
- * \author Daniel Sundfeld
  *
  * The Singleton class must implement two static methods: getInstance,
  * that creates the instance if does not exist and destroyInstance,
  * that destroy and frees the memory. This is guaranteed by creating
  * static variables from this class, maintained on a variables header
- * file.
+ * file. See file SingletonKeepVariables.h
  */
 template <class T>
 class SingletonKeep
