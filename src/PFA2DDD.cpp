2a18258490914ae11d46e46dbecda8788170296f
Daniel Sundfeld <sund@unb.br>
2014-03-27 19:25:03 -0300

    Parallel Search with multiple lists and no sync
    sync functions were called threads_num times for each expansion.
    This is complete madness.
    Using a queue architecture is MUCH BETTER.
    
    Finally, parallel code is faster then serial code. NICE

--- src/PFA2DDD.cpp
+++ src/PFA2DDD.cpp
@@ -20,59 +20,65 @@
 
 using namespace std;
 
-#define THREADS_NUM 1
+#define THREADS_NUM 4
 
 PriorityList OpenList[THREADS_NUM];
+ListType ClosedList[THREADS_NUM];
 
-atomic<int> merge_count;
-std::condition_variable merge_cond;
-std::mutex merge_mutex;
+std::mutex queue_mutex[THREADS_NUM];
+std::vector<Node> queue_nodes[THREADS_NUM];
 
-atomic<bool> end_cond;
-
-void sync_terminate()
-{
-    std::unique_lock<std::mutex> merge_lock(merge_mutex);
-    end_cond = true;
-    merge_cond.notify_all();
-}
-
-bool sync_openlist()
-{
-    std::unique_lock<std::mutex> merge_lock(merge_mutex);
-    if (end_cond)
-        return false;
-    merge_count++;
-    if (merge_count == THREADS_NUM)
-    {
-        merge_count = 0;
-        merge_cond.notify_all();
-    }
-    else
-    {
-        merge_cond.wait(merge_lock);
-        if (end_cond)
-            return false;
-    }
-    return true;
-}
-
-bool sync_skip()
-{
-    for (int i = 0; i < THREADS_NUM; i++)
-        if (sync_openlist() == false)
-            return false;
-    return true;
-}
+std::atomic<bool> end_cond;
+std::atomic<int> priorities[THREADS_NUM];
 
 /*!
- * Same a_star() function usage
+ * Add a vector of nodes \a nodes to the PriorityList \a OpenList. Use the
+ * \a ClosedList information to ignore expanded nodes.
+ * This function is a expensive function and should be called with no locks.
+ * Parallel access should never occur on \a OpenList and \a ClosedList
  */
+void pfa2ddd_enqueue(std::vector<Node> &nodes, PriorityList &OpenList, ListType &ClosedList)
+{
+    open_list_iterator o_search;
+    closed_list_iterator c_search;
+
+    for (vector<Node>::iterator it = nodes.begin() ; it != nodes.end(); ++it)
+    {
+        if ((o_search = OpenList.find(it->pos)) != OpenList.end())
+        {
+            // if score on open list is better, ignore this neighboor
+            if (it->get_g() > open_list_return_g(o_search))
+                continue;
+        }
+        if ((c_search = ClosedList.find(it->pos)) != ClosedList.end())
+        {
+            if (it->get_g() >= closed_list_return_g(c_search))
+                continue;
+            ClosedList.erase(it->pos);
+        }
+        //cout << Adding:\t" << *it << endl;
+        OpenList.enqueue(*it);
+    }
+    return;
+}
+
+//! Consume the queue with id \a tid
+void pfa2ddd_consume_queue(int tid)
+{
+    std::unique_lock<std::mutex> queue_lock(queue_mutex[tid]);
+    std::vector<Node> nodes_to_expand(queue_nodes[tid]);
+    queue_nodes[tid].clear();
+    queue_lock.unlock();
+
+    pfa2ddd_enqueue(nodes_to_expand, OpenList[tid], ClosedList[tid]);
+    return;
+}
+
+//! Execute a pfa2ddd_worker thread. This thread have id \a tid
 int pfa2ddd_worker(int tid, const Node &node_zero, bool(*is_final)(const Coord &c))
 {
-    Node current;
-    ListType ClosedList;
-    bool last_node = false;
+    Node current(0);
+    Node final_node(0);
 
     if (tid == 0)
         OpenList[tid].enqueue(node_zero);
@@ -82,86 +88,85 @@ int pfa2ddd_worker(int tid, const Node &node_zero, bool(*is_final)(const Coord &
         open_list_iterator o_search;
         closed_list_iterator c_search;
 
-        if (sync_openlist() == false)
-            break;
-
-        if (OpenList[tid].dequeue(current) == false)
+        // Start phase
+        // Check end condition
+        if (final_node.get_f() < current.get_f())
         {
-            sync_skip();
-            continue;
+            int i;
+            for (i = 0; i < THREADS_NUM; i++)
+            {
+                if (final_node.get_f() > priorities[i])
+                    break;
+            }
+            if (i == THREADS_NUM)
+            {
+                cout << "[" << tid << "] Final score:\t" << final_node << endl;
+                end_cond = true;
+                break;
+            }
         }
+        // Reduce the queue
+        pfa2ddd_consume_queue(tid);
+
+        // Dequeue phase
+        if (OpenList[tid].dequeue(current) == false)
+            continue;
+        priorities[tid] = current.get_f();
 
         // Check if better node is already found
         if ((o_search = OpenList[tid].find(current.pos)) != OpenList[tid].end())
         {
             if (current.get_g() > open_list_return_g(o_search))
-            {
-                sync_skip();
                 continue;
-            }
         }
         // Or already opened
-        if ((c_search = ClosedList.find(current.pos)) != ClosedList.end())
+        if ((c_search = ClosedList[tid].find(current.pos)) != ClosedList[tid].end())
         {
             if (current.get_g() >= closed_list_return_g(c_search))
-            {
-                sync_skip();
                 continue;
-            }
         }
 
-        //cout << "Opening node:\t" << current << endl;
-        //cout << "[" << tid << "] Abrindo: " << current << endl;
-        ClosedList[current.pos] = current;
+        //cout << "[" << tid << "] Opening node:\t" << current << endl;
+        ClosedList[tid][current.pos] = current;
 
         if (is_final(current.pos))
         {
-            cout << "MATANDO!\n";
-            last_node = true;
-            sync_terminate();
-            break;
+            if (current.get_f() < final_node.get_f())
+                final_node = current;
+            continue;
         }
-
         OpenList[tid].verifyMemory();
 
+        // Expand phase
         vector<Node> neigh[THREADS_NUM] = {} ;
         current.getNeigh(neigh, THREADS_NUM);
+
+        // Reconciliation phase
         for (int i = 0; i < THREADS_NUM; i++)
         {
-            int j = (tid + i) % THREADS_NUM;
-            if (sync_openlist() == false)
-                break;
-           //cout << "[" << tid << '/' << j << "] Turno\n";
-            for (vector<Node>::iterator it = neigh[j].begin() ; it != neigh[j].end(); ++it)
+            if (i == tid)
+                pfa2ddd_enqueue(neigh[i], OpenList[i], ClosedList[i]);
+            else
             {
-                if ((o_search = OpenList[j].find(it->pos)) != OpenList[j].end())
-                {
-                    // if score on open list is better, ignore this neighboor
-                    if (it->get_g() > open_list_return_g(o_search))
-                        continue;
-                }
-                if ((c_search = ClosedList.find(it->pos)) != ClosedList.end())
-                {
-                    if (it->get_g() >= closed_list_return_g(c_search))
-                        continue;
-                    ClosedList.erase(it->pos);
-                }
-                //cout << "[" << tid << '/' << j << "] Adding:\t" << *it << "from\t" << current << endl;
-                OpenList[j].enqueue(*it);
+                std::unique_lock<std::mutex> queue_lock(queue_mutex[i]);
+                queue_nodes[i].insert(queue_nodes[i].end(), neigh[i].begin(), neigh[i].end());
             }
         }
     }
-    if (last_node)
-        cout << "Final score:\t" << current << endl;
     return 0;
 }
 
+/*!
+ * Same a_star() function usage.
+ * Starting function to do a pfa2ddd search.
+ */
 int pfa2ddd(const Node &node_zero, bool(*is_final)(const Coord &c))
 {
     std::vector<std::thread> threads;
 
     end_cond = false;
-    merge_count = 0;
+    for (int i = 0; i < THREADS_NUM; ++i)
+        priorities[i] = std::numeric_limits<int>::max();
 
     for (int i = 0; i < THREADS_NUM; ++i)
         threads.push_back(std::thread(pfa2ddd_worker, i, node_zero, is_final));
