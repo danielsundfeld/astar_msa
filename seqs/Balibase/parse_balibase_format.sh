c96257abd54e721f660f7ea0ea36ef79f1467eab
Daniel Sundfeld <sund@unb.br>
2015-01-22 08:19:26 -0200

    Test sequences

---
+++ seqs/Balibase/parse_balibase_format.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+seqs=$(cat $1 | grep -e "  *1  " | awk '{print $1}')
+
+for i in $seqs; do
+    echo ">"$i;
+    lin=$(cat $1 | grep $i | awk '{print $3}' | sed "s/\.//g")
+    echo "${lin^^}" | xargs -n1 echo -n
+    echo
+done
