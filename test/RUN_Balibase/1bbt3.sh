d488c62a40344d6b9f93b98d7384cf52b765af19
Daniel Sundfeld <sund@unb.br>
2015-01-27 13:33:53 +0100

    SBATCH header to BalibaseRun scripts

--- test/RUN_Balibase/1bbt3.sh
+++ test/RUN_Balibase/1bbt3.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+#SBATCH -J astar           # job name
+#SBATCH -o Pipeline.o%j       # output and error file name (%j expands to jobID)
+#SBATCH -n 1              # total number of mpi tasks requested
+#SBATCH -N 1             # total number of mpi tasks requested
+#SBATCH -p largemem     # queue (partition) -- normal, development, etc.
+#SBATCH -t 24:00:00        # run time (hh:mm:ss) - 1.5 hours
+#SBATCH --mail-type=begin  # email me when the job starts
+#SBATCH --mail-type=end    # email me when the job finishes
+#SBATCH -A TG-ASC130023
+module swap intel gcc/4.7.1
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 SEQ="../../seqs/Balibase/Ref1/3_medium_low_id/1bbt3.fasta"
 CMD="../../src/msa_pastar"
