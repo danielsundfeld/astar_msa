7e2612a9fc57954fbd6fad65c3ebd7aed928a4cd
Daniel Sundfeld <sund@unb.br>
2015-02-04 12:15:53 +0100

    AStar: AStar options struct
    New struct to have all options to the AStar algorithm.
    Force quit is the new option.

--- src/AStar.h
+++ src/AStar.h
@@ -9,5 +9,23 @@
 #include "Coord.h"
 #include "Node.h"
 
+struct AStarOpt {
+    /*!
+     * Calling all destructors in the OpenList and the Global Memory might be
+     * really time consuming. If only one alignment is done, we might exit(0)
+     * and save some time. This option may be changed for memcheck purposes.
+     */
+    bool force_quit;
+
+    AStarOpt()
+    {
+        force_quit = true;
+    }
+    AStarOpt(bool force)
+    {
+        force_quit = force;
+    }
+};
+
 template <int N> int a_star(const Node<N> &node_zero, const Coord<N> &coord_final);
 #endif
