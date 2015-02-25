d909bb24bd0d9aca8dfafe009c15a08e73ef0b9e
Daniel Sundfeld <sund@unb.br>
2015-02-25 12:54:43 +0100

    RUN_Benchmark scripts and sequences

---
+++ test/RUN_Benchmark/2ack_9.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -J pastar_2ack_9           # job name
+#SBATCH -o Pipeline.o%j       # output and error file name (%j expands to jobID)
+#SBATCH -n 1              # total number of mpi tasks requested
+#SBATCH -N 1             # total number of mpi tasks requested
+#SBATCH -p largemem     # queue (partition) -- normal, development, etc.
+#SBATCH -t 06:00:00        # run time (hh:mm:ss) - 1.5 hours
+#SBATCH --mail-type=begin  # email me when the job starts
+#SBATCH --mail-type=end    # email me when the job finishes
+#SBATCH -A TG-ASC130023
+module swap intel gcc/4.7.1
+
+SEQ="../../seqs/Benchmark/2ack_cutted.fasta"
+CMD="../../src/msa_pastar"
+
+THREADS="-t 9"
+HASH="-y FZORDER"
+HASH_SHIFT="-s 12"
+OPT="$THREADS $HASH $HASH_SHIFT"
+
+
+/usr/bin/time -v $CMD $OPT $SEQ > 2ack_9.output 2>&1
---
+++ test/RUN_Benchmark/glg_21.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -J pastar_glg_21           # job name
+#SBATCH -o Pipeline.o%j       # output and error file name (%j expands to jobID)
+#SBATCH -n 1              # total number of mpi tasks requested
+#SBATCH -N 1             # total number of mpi tasks requested
+#SBATCH -p largemem     # queue (partition) -- normal, development, etc.
+#SBATCH -t 06:00:00        # run time (hh:mm:ss) - 1.5 hours
+#SBATCH --mail-type=begin  # email me when the job starts
+#SBATCH --mail-type=end    # email me when the job finishes
+#SBATCH -A TG-ASC130023
+module swap intel gcc/4.7.1
+
+SEQ="../../seqs/Benchmark/glg_cutted.fasta"
+CMD="../../src/msa_pastar"
+
+THREADS="-t 21"
+HASH="-y FZORDER"
+HASH_SHIFT="-s 12"
+OPT="$THREADS $HASH $HASH_SHIFT"
+
+
+/usr/bin/time -v $CMD $OPT $SEQ > glg_21.output 2>&1
