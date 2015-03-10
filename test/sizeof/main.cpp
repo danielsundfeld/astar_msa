b5df659975e0d61f50c438e69e31da574c8279dd
Daniel Sundfeld <sund@unb.br>
2015-03-10 13:48:17 +0100

    Sizeof test

---
+++ test/sizeof/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Coord.h"
+#include "Node.h"
+
+using namespace std;
+
+int main(void)
+{
+    std::cout << "sizeof Node: " << sizeof(Node) << std::endl;
+    return 0;
+}
