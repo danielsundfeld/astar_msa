30f48ded1e679605495aef3ec2f045c4d4d888b2
Daniel Sundfeld <sund@unb.br>
2015-05-05 14:35:07 +0200

    Cost: Do not use char as arrays subscripts

--- src/Cost.h
+++ src/Cost.h
@@ -12,6 +12,36 @@ class Cost {
         Cost();
         enum { GapExtension = 30, GapOpen = 30, GapGap = GapOpen };
 
+        //! Do not use char as array subscripts, this enum 'cast' them to integer
+        enum {
+            A = 'A',
+            B = 'B',
+            C = 'C',
+            D = 'D',
+            E = 'E',
+            F = 'F',
+            G = 'G',
+            H = 'H',
+            I = 'I',
+            J = 'J',
+            K = 'K',
+            L = 'L',
+            M = 'M',
+            N = 'N',
+            O = 'O',
+            P = 'P',
+            Q = 'Q',
+            R = 'R',
+            S = 'S',
+            T = 'T',
+            U = 'U',
+            V = 'V',
+            W = 'W',
+            X = 'X',
+            Y = 'Y',
+            Z = 'Z'
+        };
+
         static int cost(const char r, const char l);
 
     private:
