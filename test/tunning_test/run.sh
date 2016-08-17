7cb69dbfe6e86d11f5016f2bad9d3c2d1ed54fd8
Daniel Sundfeld <sundfeld@rth.dk>
2016-08-17 23:42:03 +0200

    Wrong script

--- test/tunning_test/run.sh
+++ test/tunning_test/run.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 cd ../../src/
-#make -B THREADS=1 NO_LIB_BOOST=1
-#strace -ve wait4 /usr/bin/time -v ./msa_pastar ../seqs/3/synthetic_easy.fasta                >> pastar_TST1_dictionary_dynamic_03 2>&1
-#strace -ve wait4 /usr/bin/time -v ./msa_pastar ../seqs/4/3pmg_ref1.fasta                     >> pastar_TST1_dictionary_dynamic_04 2>&1
-#strace -ve wait4 /usr/bin/time -v ./msa_pastar ../seqs/5/EASY_instances/synthetic_easy.fasta >> pastar_TST1_dictionary_dynamic_05 2>&1
-#strace -ve wait4 /usr/bin/time -v ./msa_pastar ../seqs/22/PF07708_seed.txt                   >> pastar_TST1_dictionary_dynamic_22 2>&1
+make -B THREADS=1 NO_LIB_BOOST=1
+strace -ve wait4 /usr/bin/time -v ./msa_pastar ../seqs/3/synthetic_easy.fasta                >> pastar_TST1_dictionary_dynamic_03 2>&1
+strace -ve wait4 /usr/bin/time -v ./msa_pastar ../seqs/4/3pmg_ref1.fasta                     >> pastar_TST1_dictionary_dynamic_04 2>&1
+strace -ve wait4 /usr/bin/time -v ./msa_pastar ../seqs/5/EASY_instances/synthetic_easy.fasta >> pastar_TST1_dictionary_dynamic_05 2>&1
+strace -ve wait4 /usr/bin/time -v ./msa_pastar ../seqs/22/PF07708_seed.txt                   >> pastar_TST1_dictionary_dynamic_22 2>&1
 
-#make -B THREADS=1
-#strace -ve wait4 /usr/bin/time -v ./msa_pastar ../seqs/3/synthetic_easy.fasta                >> pastar_TST2_multiindex_dynamic_03 2>&1
-#strace -ve wait4 /usr/bin/time -v ./msa_pastar ../seqs/4/3pmg_ref1.fasta                     >> pastar_TST2_multiindex_dynamic_04 2>&1
-#strace -ve wait4 /usr/bin/time -v ./msa_pastar ../seqs/5/EASY_instances/synthetic_easy.fasta >> pastar_TST2_multiindex_dynamic_05 2>&1
-#strace -ve wait4 /usr/bin/time -v ./msa_pastar ../seqs/22/PF07708_seed.txt                   >> pastar_TST2_multiindex_dynamic_22 2>&1
-#
+make -B THREADS=1
+strace -ve wait4 /usr/bin/time -v ./msa_pastar ../seqs/3/synthetic_easy.fasta                >> pastar_TST2_multiindex_dynamic_03 2>&1
+strace -ve wait4 /usr/bin/time -v ./msa_pastar ../seqs/4/3pmg_ref1.fasta                     >> pastar_TST2_multiindex_dynamic_04 2>&1
+strace -ve wait4 /usr/bin/time -v ./msa_pastar ../seqs/5/EASY_instances/synthetic_easy.fasta >> pastar_TST2_multiindex_dynamic_05 2>&1
+strace -ve wait4 /usr/bin/time -v ./msa_pastar ../seqs/22/PF07708_seed.txt                   >> pastar_TST2_multiindex_dynamic_22 2>&1
+
 make clean
 git checkout v1.1
 cd ..
