55d3bf9099741aa0e5bcacea89a79f02a0244def
Daniel Sundfeld <sund@unb.br>
2016-08-19 18:58:58 -0300

    Sythetic typo

--- test/RUN_Cutted/synthetic_long_32.sh
+++ test/RUN_Cutted/synthetic_long_32.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -J pastar_synthethic_long_32           # job name
+#SBATCH -J pastar_synthetic_long_32           # job name
 #SBATCH -o Pipeline.o%j       # output and error file name (%j expands to jobID)
 #SBATCH -n 1              # total number of mpi tasks requested
 #SBATCH -N 1             # total number of mpi tasks requested
@@ -10,7 +10,7 @@
 #SBATCH -A TG-ASC130023
 module swap intel gcc/4.7.1
 
-SEQ="../../seqs/Benchmark/synthethic_long.fasta"
+SEQ="../../seqs/Benchmark/synthetic_long.fasta"
 CMD="../../bin/msa_pastar"
 
 THREADS="-t 32"
@@ -19,4 +19,4 @@ HASH_SHIFT="-s 12"
 OPT="$THREADS $HASH $HASH_SHIFT"
 
 
-strace -ve wait4 /usr/bin/time -v $CMD $OPT $SEQ >> synthethic_long_pastar_32.output 2>&1
+strace -ve wait4 /usr/bin/time -v $CMD $OPT $SEQ >> synthetic_long_pastar_32.output 2>&1
