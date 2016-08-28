8c39d21bbd2999e5c5926d859a1a9f899408254d
Daniel Sundfeld <sund@unb.br>
2016-08-28 20:40:27 -0300

    Updating Balibase scripts

--- test/RUN_Balibase/1eft.sh
+++ test/RUN_Balibase/1eft.sh
@@ -8,10 +8,10 @@
 #SBATCH --mail-type=begin  # email me when the job starts
 #SBATCH --mail-type=end    # email me when the job finishes
 #SBATCH -A TG-ASC130023
-module swap intel gcc/4.7.1
+module swap intel gcc/4.9.1
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 SEQ="../../seqs/Balibase/Ref1/7_long_medium_id/1eft.fasta"
-CMD="../../src/msa_pastar"
+CMD="../../bin/msa_pastar"
 
 THREADS="-t 32"
 HASH="-y FZORDER"
@@ -20,4 +20,4 @@ OPT="$THREADS $HASH $HASH_SHIFT"
 
 cd $DIR"/../../seqs/Balibase"
 
-/usr/bin/time -v $CMD $OPT $SEQ > $SEQ.output 2>&1
+strace -ve wait4 /usr/bin/time -v $CMD $OPT $SEQ >> $SEQ.output 2>&1
