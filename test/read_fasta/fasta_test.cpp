78d5770022f7370541fda6b4c923e10abd781813
Daniel Sundfeld <danielsundfeld@gmail.com>
2018-11-14 23:23:15 -0200

    New read_fast test

---
+++ test/read_fasta/fasta_test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#include "read_fasta.h"
+#include "Sequences.h"
+
+int main(void)
+{
+    if (read_fasta_file("arq.fasta"))
+    {
+        std::cerr << "Error loading fasta arq.fasta\n";
+        return -1;
+    }
+    std::cout << Sequences::get_seq_num();
+    for (int i = 0; i < Sequences::get_seq_num(); ++i)
+    {
+        std::cout << " " << Sequences::getInstance()->get_seq(i).length();
+    }
+    std::cout << std::endl;
+    return 0;
+}
